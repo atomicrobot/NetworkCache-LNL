c39279a9d5ecc589fffad2049f8e81b0b553f4d8
Adam Toennis <adam@atomicrobot.io>
2018-02-18 12:40:36 -0500

    Added tests for etag.

--- app/src/test/java/io/atomicrobot/httpcachepresentation/HttpCacheTests.kt
+++ app/src/test/java/io/atomicrobot/httpcachepresentation/HttpCacheTests.kt
@@ -1,25 +1,48 @@
 package io.atomicrobot.httpcachepresentation
 
+import okhttp3.Cache
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.util.logging.Level
+import java.util.logging.Logger
 
 
 class HttpCacheTests {
+    @JvmField
+    @Rule
+    val folder = TemporaryFolder()
+
     lateinit var server: MockWebServer
     lateinit var client: OkHttpClient
+    lateinit var baseUrl: HttpUrl
+    lateinit var cache: Cache
 
     @Before
     fun setup() {
+        Logger.getLogger(MockWebServer::class.java.name).level = Level.OFF
+        val cacheDir = folder.root
+
         server = MockWebServer()
         server.start(8000)
+        baseUrl = server.url("/v1/model/")
+        cache = Cache(cacheDir, DISK_CACHE_SIZE.toLong())
 
-        client = OkHttpClient()
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        client = OkHttpClient().newBuilder()
+//                .addInterceptor(logging)
+                .cache(cache)
+                .build()
     }
 
     @After
@@ -29,7 +52,6 @@ class HttpCacheTests {
 
     @Test
     fun testMockServerShouldWork() {
-        val baseUrl = server.url("/v1/model/")
         server.enqueue(MockResponse().setBody("hello, world!"))
         server.enqueue(MockResponse().setBody("hello, again!"))
         server.enqueue(MockResponse().setBody("hello, one more time!"))
@@ -41,8 +63,74 @@ class HttpCacheTests {
         val response2 = client.newCall(request).execute()
         val response3 = client.newCall(request).execute()
 
-        Assert.assertEquals("hello, world!", response1.body()?.string())
-        Assert.assertEquals("hello, again!", response2.body()?.string())
-        Assert.assertEquals("hello, one more time!", response3.body()?.string())
+        assertEquals("hello, world!", response1.body()?.string())
+        assertEquals("hello, again!", response2.body()?.string())
+        assertEquals("hello, one more time!", response3.body()?.string())
+    }
+
+    @Test
+    fun testMaxAge() {
+        server.enqueue(MockResponse().setHeader("cache-control", "max-age=1").setBody("First Response"))
+        server.enqueue(MockResponse().setHeader("cache-control", "max-age=2").setBody("Second Response"))
+        val request = Request.Builder()
+                .url(baseUrl)
+                .build()
+
+        val response1 = client.newCall(request).execute()
+        assertEquals(1, cache.requestCount())
+        assertEquals(1, cache.networkCount())
+        assertEquals(0, cache.hitCount())
+        assertEquals("First Response", response1.body()?.string())
+
+        val response2 = client.newCall(request).execute()
+        assertEquals(2, cache.requestCount())
+        assertEquals(1, cache.networkCount())
+        assertEquals(1, cache.hitCount())
+        assertEquals("First Response", response2.body()?.string())
+
+        // Wait until the max-age expires, 1 second
+        Thread.sleep(1000)
+
+        val response3 = client.newCall(request).execute()
+        assertEquals(3, cache.requestCount())
+        assertEquals(2, cache.networkCount())
+        assertEquals(1, cache.hitCount())
+        assertEquals("Second Response", response3.body()?.string())
+    }
+
+    // Also referred to as conditionally cached
+    @Test
+    fun testEtag() {
+        server.enqueue(MockResponse()
+                .setHeader("ETag", "v1")
+                .setResponseCode(200)
+                .setBody("First Response"))
+        server.enqueue(MockResponse()
+                .setResponseCode(304))
+        val request = Request.Builder()
+                .url(baseUrl)
+                .build()
+
+        val response1 = client.newCall(request).execute()
+        assertEquals(1, cache.requestCount())
+        assertEquals(1, cache.networkCount())
+        assertEquals(0, cache.hitCount())
+        assertEquals("First Response", response1.body()?.string())
+
+// OkHTTP is adding the If-None-Match header internally
+//        val request2 = Request.Builder()
+//                .url(baseUrl)
+//                .header("If-None-Match", "v1")
+//                .build()
+
+        val response2 = client.newCall(request).execute()
+        assertEquals(2, cache.requestCount())
+        assertEquals(2, cache.networkCount())
+        assertEquals(1, cache.hitCount())
+        assertEquals("First Response", response2.body()?.string())
+    }
+
+    companion object {
+        private const val DISK_CACHE_SIZE = 50 * 1024 * 1024 // 50MB
     }
 }
